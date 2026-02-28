da0ca973a3fc14dca90fbd7d4a76fade65a1ae72
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-28 10:31:30 +0530

    feat(auth-service): introduce UserPrincipal for authentication context  - Implemented UserDetails - Mapped UserCredentials to security principal - Added role-to-authority conversion - Included account status handling

---
+++ auth-service/src/main/java/com/example/auth/security/UserPrincipal.java
@@ -0,0 +1,55 @@
+package com.example.auth.security;
+
+import com.example.auth.entity.UserCredentials;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private UUID id;
+    private String username;
+    private String password;
+    private String role;
+    private boolean isActive;
+
+    public static UserPrincipal create(UserCredentials user){
+        return new UserPrincipal(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole(),
+                user.isActive()
+        );
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
