69eb8135d3d48673a0c1ce2633e19a30c22622d1
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-02-28 09:53:29 +0530

    Finish creating UserCredentialRepo with expected methods

--- auth-service/src/main/java/com/example/auth/repo/UserCredentialsRepo.java
+++ auth-service/src/main/java/com/example/auth/repo/UserCredentialsRepo.java
@@ -1,4 +1,17 @@
 package com.example.auth.repo;
 
-public class UserCredentialsRepo {
+import com.example.auth.entity.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserCredentialsRepo extends JpaRepository<UserCredentials, UUID> {
+    Optional<UserCredentials> findByUsername(String username);
+    Optional<UserCredentials> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
