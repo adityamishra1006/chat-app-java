7532ff7ac48489cd9b6ca3c57ddd7a09ef6d4885
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-03-02 21:26:24 +0530

    Created JwtUtils

---
+++ api-gateway/src/main/java/com/aditya/gateway/security/JwtUtils.java
@@ -0,0 +1,61 @@
+package com.aditya.gateway.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token){
+        try{
+            getClaims(token);
+            return true;
+        }catch (JwtException | IllegalArgumentException e){
+            log.error("Invalid JWT token: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public UUID getUserIdFromToken(String token){
+        String subject = getClaims(token).getSubject();
+        return UUID.fromString(subject);
+    }
+
+    public String getUsernameFromToken(String token){
+        return getClaims(token).get("username", String.class);
+    }
+
+    public String getRoleFromToken(String token){
+        return getClaims(token).get("role", String.class);
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
