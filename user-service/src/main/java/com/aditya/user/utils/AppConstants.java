f5d247b7366ebb107c5b2c9f9e63e70d4b79ada9
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-03-02 23:30:31 +0530

    feat(user-service): add AppConstants utility class
    - Created AppConstants class to store application-wide constants
    - Added header constants for X-User-Id, X-Username, and X-User-Role
    - Added USER_NOT_FOUND message constant
    - Prevented instantiation with private constructor

---
+++ user-service/src/main/java/com/aditya/user/utils/AppConstants.java
@@ -0,0 +1,14 @@
+package com.aditya.user.utils;
+
+public class AppConstants {
+    private AppConstants(){
+
+    }
+
+    public static final String HEADER_USER_ID = "X-User-Id";
+    public static final String HEADER_USERNAME = "X-Username";
+    public static final String HEADER_USER_ROLE = "X-User-Role";
+
+    public static final String USER_NOT_FOUND = "User not found";
+
+}
