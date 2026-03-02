2fda0b5c5ba09a9ffa3cd5c8eaac4c0e9daa154b
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-03-02 23:20:29 +0530

    Some checks

--- api-gateway/src/main/java/com/aditya/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/aditya/gateway/ApiGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.aditya.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
