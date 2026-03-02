72cf678d9c657ee82274db60a3da84fee8602493
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-03-02 21:46:27 +0530

    Created user entity

---
+++ user-service/src/main/java/com/aditya/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.aditya.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 200)
+    private String bio;
+
+    private String avatarUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
